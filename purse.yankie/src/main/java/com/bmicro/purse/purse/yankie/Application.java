380eb4c58d0caf24bf0dc07aa9d2fd463b92711f
verdugox <luisandresverdugox@gmail.com>
2023-01-31 21:59:27 -0500

    Subiendo Micro Purse Yankie

---
+++ purse.yankie/src/main/java/com/bmicro/purse/purse/yankie/Application.java
@@ -0,0 +1,13 @@
+package com.bmicro.purse.purse.yankie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ purse.yankie/src/test/java/com/bmicro/purse/purse/yankie/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmicro.purse.purse.yankie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
